6da5538301d6c5fc537695809718031c8b298b04
luce_gi <giovanni.lucente@dlr.de>
2025-01-20 17:51:14 +0100

    bug fixed

--- src/decision_maker_infrastructure.cpp
+++ src/decision_maker_infrastructure.cpp
@@ -31,6 +31,10 @@ DecisionMakerInfrastructure::run()
 {
   update_dynamic_subscriptions();
   dynamics::remove_old_participants( latest_traffic_participant_set, 0.5, this->now().seconds() );
+  if (road_map.has_value())
+  {
+    compute_routes_for_traffic_participant_set(latest_traffic_participant_set, road_map.value());
+  }
   all_vehicles_follow_routes();
   if( debug_mode_active )
     print_debug_info();
@@ -43,6 +47,7 @@ DecisionMakerInfrastructure::all_vehicles_follow_routes()
 {
   multi_agent_PID_planner.plan_trajectories( latest_traffic_participant_set, command_limits );
   publisher_planned_traffic->publish( dynamics::conversions::to_ros_msg( latest_traffic_participant_set ) );
+  
 }
 
 void
@@ -142,9 +147,9 @@ DecisionMakerInfrastructure::compute_routes_for_traffic_participant_set( dynamic
 {
   for( auto& pair : traffic_participant_set )
   {
-    if( pair.second.goal_point.has_value() && pair.second.route.has_value() )
+    if( pair.second.goal_point.has_value() )
     {
-      pair.second.route.value() = road_map.get_route( pair.second.state, pair.second.goal_point.value() );
+      pair.second.route = road_map.get_route( pair.second.state, pair.second.goal_point.value() );
     }
     else
     {
@@ -165,16 +170,21 @@ DecisionMakerInfrastructure::traffic_participant_callback( const adore_ros2_msgs
 void
 DecisionMakerInfrastructure::update_dynamic_subscriptions()
 {
-  auto topic_names_and_types = get_topic_names_and_types();
+  auto       topic_names_and_types = get_topic_names_and_types();
+  std::regex valid_topic_regex( R"(^/([^/]+)/traffic_participant$)" );
+  std::regex valid_type_regex( R"(^adore_ros2_msgs/msg/TrafficParticipant$)" );
+
   for( const auto& topic : topic_names_and_types )
   {
     const std::string&              topic_name = topic.first;
     const std::vector<std::string>& types      = topic.second;
 
-    if( topic_name.find( "/traffic_participant" ) != std::string::npos
-        && std::find( types.begin(), types.end(), "adore_ros2_msgs/msg/TrafficParticipant" ) != types.end() )
+    std::smatch match;
+    if( std::regex_match( topic_name, match, valid_topic_regex )
+        && std::any_of( types.begin(), types.end(),
+                        [&]( const std::string& type ) { return std::regex_match( type, valid_type_regex ); } ) )
     {
-      std::string vehicle_namespace = topic_name.substr( 1, topic_name.find( "/traffic_participant" ) - 1 );
+      std::string vehicle_namespace = match[1].str();
 
       // Skip subscribing to own namespace
       if( vehicle_namespace == std::string( get_namespace() ).substr( 1 ) )
